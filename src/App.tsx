647ce95f642e270690076b6eac5f382865e9a90a
Rasmus Schultz <rs@abtion.com>
2022-07-11 15:23:03 +0200

    for the sake of discussion, simplify with a mock example

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import prepareColorVariables from "./utils/prepareColorVariables"
 import colors from "../colors.json"
 
-const cssVariables = prepareColorVariables(colors).cssVariables
+const lol = prepareColorVariables(colors)
 
-console.log(cssVariables)
+console.log(lol)
 
 export default function App(): JSX.Element {
   return (
--- src/utils/prepareColorVariables.d.ts
+++ src/utils/prepareColorVariables.d.ts
@@ -2,28 +2,4 @@ export as namespace prepareColorVariables
 
 export default prepareColorVariables
 
-declare function prepareColorVariables(
-  jsonColors: prepareColorVariables.JsonColors
-): prepareColorVariables.PreparedColorVariables
-
-declare namespace prepareColorVariables {
-  export interface JsonColors {
-    [name: string]: string | Record<string, string>
-  }
-
-  export interface CssVariableDefinition {
-    [name: string]: string
-  }
-
-  export interface TailwindConfig {
-    [name: string]: string | Record<string, string>
-  }
-
-  export interface PreparedColorVariables {
-    cssVariables: CssVariableDefinition
-    tailwindConfig: TailwindConfig
-  }
-
-  export const defaultName: string
-  export let defaultLength: number
-}
+declare function prepareColorVariables(_: any): string;
